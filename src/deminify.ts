49610697a1e1b5c234c600fa43f004e7c05db205
Jared Hughes <jahughes241@gmail.com>
2022-11-17 03:22:31 -0800

    Handle vendor libraries

--- src/deminify.ts
+++ src/deminify.ts
@@ -2,8 +2,8 @@ import { parse } from 'acorn'
 import * as estree from 'estree'
 import estraverse from 'estraverse'
 import TotalTransformer from './TotalTransformer'
-
 import { rmdir, readFile } from 'fs/promises'
+import { emit } from './lib'
 
 export interface DeminifyOptions {
   dry: boolean
@@ -26,12 +26,26 @@ export default async function deminifyFile(
     })
   }
 
-  const parsed = parse(source, {
-    ecmaVersion: 6,
-  }) as estree.Node
-  const transformer = new TotalTransformer(parsed, opts)
-  estraverse.traverse(parsed, {
-    enter: (node, parent) => transformer.transformEnter(node, parent),
-    leave: (node, parent) => transformer.transformLeave(node, parent),
-  })
+  for (const line of source.split('\n')) {
+    if (line.startsWith('!function(')) {
+      // !function() defines are typically some sort of vendor library, e.g. jQuery
+      // probably imported as `import "jquery"` instead of `import jquery from "jquery"`
+      // Assume quotes are correctly matched etc, and this gives the path for the whole line
+      const match = line.match(/define\(['"]([^'"]+)['"]/)
+      if (match !== null) {
+        const definePath = match[1]
+        emit(definePath, line, opts)
+      }
+    } else {
+      // regular define module
+      const parsed = parse(line, {
+        ecmaVersion: 6,
+      }) as estree.Node
+      const transformer = new TotalTransformer(parsed, opts)
+      estraverse.traverse(parsed, {
+        enter: (node, parent) => transformer.transformEnter(node, parent),
+        leave: (node, parent) => transformer.transformLeave(node, parent),
+      })
+    }
+  }
 }
